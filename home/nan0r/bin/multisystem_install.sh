f937b3456ff3746e6e69eb11cc428bf101d53260
Launay <launay.ronan@gmail.com>
2014-10-29 16:36:08 +0100

    Update multisystem_install.sh

--- home/nan0r/bin/multisystem_install.sh
+++ home/nan0r/bin/multisystem_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-apt-add-repository 'deb http://liveusb.info/multisystem/depot all main'
-wget -q http://liveusb.info/multisystem/depot/multisystem.asc -O- | sudo apt-key add -
+sudo apt-add-repository 'deb http://liveusb.info/multisystem/depot all main'
+sudo wget -q http://liveusb.info/multisystem/depot/multisystem.asc -O- | sudo apt-key add -
 sudo apt-get install multisystem
 sudo usermod -a -G adm $USER
 exit
