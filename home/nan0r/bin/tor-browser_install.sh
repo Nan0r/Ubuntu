c5839747298775ca5034dcb70d1ce16d8f3f0659
Launay <launay.ronan@gmail.com>
2014-10-29 16:07:48 +0100

    Update tor-browser_install.sh

--- home/nan0r/bin/tor-browser_install.sh
+++ home/nan0r/bin/tor-browser_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo add-apt-repository ppa:upubuntu-com/tor64
+sudo add-apt-repository -y ppa:upubuntu-com/tor64
 sudo apt-get update 
 sudo apt-get install tor-browser 
 sudo chown $USER -R ~/.tor-browser/
