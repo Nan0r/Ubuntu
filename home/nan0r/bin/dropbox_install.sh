08627a62cc431d0a7ad9b0e4727c398912659d02
Launay <launay.ronan@gmail.com>
2014-10-29 16:33:46 +0100

    Update dropbox_install.sh

--- home/nan0r/bin/dropbox_install.sh
+++ home/nan0r/bin/dropbox_install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt-add-repository 'deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main'
 apt-key adv --keyserver pgp.mit.edu --recv-keys 3565780E
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 3565780E (under firewall)
+# If you under firewall 
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 3565780E
 sudo apt-get update
 sudo apt-get install dropbox 
 exit
