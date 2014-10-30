1b90744414dd70d15766dfc2169efbbc1c3dc754
Launay <launay.ronan@gmail.com>
2014-10-30 10:24:24 +0100

    Update spotify_install.sh

--- home/nan0r/bin/spotify_install.sh
+++ home/nan0r/bin/spotify_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-add-repository 'deb http://repository.spotify.com stable non-free'
+sudo apt-add-repository -y 'deb http://repository.spotify.com stable non-free'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 # If you under firewall
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
