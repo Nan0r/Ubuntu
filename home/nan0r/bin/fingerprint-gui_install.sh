5c8a4d97b689a16d0f9832629ac41ca0cc206be8
Launay <launay.ronan@gmail.com>
2014-10-30 10:23:30 +0100

    Update fingerprint-gui_install.sh

--- home/nan0r/bin/fingerprint-gui_install.sh
+++ home/nan0r/bin/fingerprint-gui_install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-sudo add-apt-repository ppa:fingerprint/fingerprint-gui
+sudo add-apt-repository -y ppa:fingerprint/fingerprint-gui
 sudo apt-get update
 sudo apt-get install libbsapi policykit-1-fingerprint-gui fingerprint-gui
 exit
