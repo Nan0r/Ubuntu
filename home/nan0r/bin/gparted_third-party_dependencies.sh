715bdc36245a9eca24753ccf554e6ee24bff138a
Launay <launay.ronan@gmail.com>
2014-10-30 10:19:38 +0100

    gparted_third-party_dependencies.sh

---
+++ home/nan0r/bin/gparted_third-party_dependencies.sh
@@ -0,0 +1,31 @@
+# btrfs : 
+btrfs-tools 
+# ext2 / ext3 / ext4 : 
+e2fsprogs 
+# f2fs : 
+f2fs-tools 
+# fat16 / fat32 : 
+dosfstools 
+mtools 
+# hfs : 
+hfsutils
+# hfs+ : 
+hfsprogs 
+jfs : 
+jfsutils 
+# linux-swap : 
+util-linux 
+# lvm2 pv : 
+lvm2 
+# nilfs2 : 
+nilfs-utils 
+# ntfs : 
+ntfsprogs 
+ntfs-3g 
+# reiser4 : 
+reiser4progs 
+# reiserfs : 
+reiserfsprogs 
+# xfs : 
+xfsprogs
+xfsdump
